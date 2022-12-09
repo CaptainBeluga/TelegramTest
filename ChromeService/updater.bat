481e6445699ca43bd530d5e773e5d2b46be6cf55
Cap Beluga <simone.pata27@gmail.com>
2022-12-09 11:06:31 +0100

    MAIN

---
+++ ChromeService/updater.bat
@@ -0,0 +1,19 @@
+taskkill /IM python.exe /F
+
+ipconfig/flushdns
+
+del "C:\Users\Beluga\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ChromeUpdater.lnk"
+
+echo S | del C:\Users\Beluga\AppData\Local\Programs\ChromeService\chrome
+
+echo S | del C:\Users\Beluga\AppData\Local\Programs\ChromeService\chrome\__pycache__
+
+rmdir C:\Users\Beluga\AppData\Local\Programs\ChromeService\chrome\__pycache__
+
+rmdir C:\Users\Beluga\AppData\Local\Programs\ChromeService\chrome
+
+echo S | del C:\Users\Beluga\AppData\Local\Programs\ChromeService
+
+rmdir C:\Users\Beluga\AppData\Local\Programs\ChromeService
+
+del C:\Users\Beluga\AppData\Local\Programs\updater.bat
